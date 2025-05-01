6f53d97f0641ac5a6cff5e8923e8be30e1f43e93
ilyasayapin <kite@nxt.ru>
2025-05-01 15:17:29 +0300

    Lesson 7. Homework

---
+++ src/VehicleHomework.java
@@ -0,0 +1,10 @@
+public class VehicleHomework {
+
+    int maxSpeed;
+    int cargoCapacity;
+
+    public VehicleHomework (int maxSpeed, int cargoCapacity) {
+        this.maxSpeed = maxSpeed;
+        this.cargoCapacity = cargoCapacity;
+    }
+}
