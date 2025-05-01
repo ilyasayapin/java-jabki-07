a6d19af54a4f4a2ae44c6fde7d115ec825a976a1
ilyasayapin <kite@nxt.ru>
2025-05-01 13:09:01 +0300

    Lesson 7. Homework

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+
+    private int accountNumber = (int) (Math.random() * 100 + 1);
+    private double balance;
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Введите положительное число");
+        } else if (this.balance <= amount) {
+            throw new IllegalArgumentException("Баланс не может уйти в минус");
+        }
+        this.balance -= amount;
+    }
+
+
+}
