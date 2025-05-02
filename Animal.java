1173c1ca34743d102c576c3401ed6435994b0c82
ilyasayapin <kite@nxt.ru>
2025-05-02 15:19:00 +0300

    Lesson 7. Homework

---
+++ Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Издает звук");
+    }
+
+    public void eat() {
+        System.out.println(name + " ест");
+    }
+
+
+
+}
---
+++ src/Lion.java
@@ -0,0 +1,6 @@
+public class Lion extends Animal {
+
+    public Animal(String name, int age) {
+        super(name, age);
+    }
+}
